1052c7c449372d501d69779fe782fd96b58ee6a6
Marcel Pfotenhauer <m.pfotenhauer@xceptance.net>
2018-02-08 15:14:08 +0100

    updated pom to run on command line and renamed a test case

--- src/test/java/posters/cucumber/tests/AllFeaturesTest.java
+++ src/test/java/posters/cucumber/tests/AllFeaturesTest.java
@@ -7,14 +7,11 @@ import com.xceptance.neodymium.NeodymiumCucumberRunner;
 import cucumber.api.CucumberOptions;
 
 @RunWith(NeodymiumCucumberRunner.class)
-@CucumberOptions(features = "src/test/java/posters/cucumber/features", glue = "posters", tags =
+@CucumberOptions(features = "src/test/java/posters/cucumber/features", glue = "posters", tags = "not @Skip", plugin =
 {
-  "not @Skip"
-}, plugin =
-{
-  "pretty", // console output
+  "pretty", // console // output
   "html:target/cucumber-report/", // html report
 })
-public class RunAllFeaturesTest
+public class AllFeaturesTest
 {
 }
