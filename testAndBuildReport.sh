c89dc21ce152f5b5e89c737e7e3f8cf7ad42fab8
Uwe Friedel <u.friedel@xceptance.net>
2021-03-16 12:21:52 +0100

    Press enter to continue added in sh script

--- testAndBuildReport.sh
+++ testAndBuildReport.sh
@@ -2,15 +2,17 @@
 
 # If a windows user wants to try this script and has git installed, goto windows explorer, 
 # right click the project directory, click "Git Bash Here", execute this script.
+# Or just double click on the file.
 
 # check prerequistes
 if ! which mvn > /dev/null 2>&1; then
   echo "There is no maven installed. Apache Maven 3 is required to run."
+  read -p "Press enter to continue"
   exit 1
 fi
 if ! mvn --version | grep 'Apache Maven 3'; then
-  echo "Apache Maven 3 is required to run. Recheck following:"
-  mvn --version
+  echo "Apache Maven 3 is required to run. A lower version is installed."
+  read -p "Press enter to continue"
   exit 1
 fi
 
